fc9cc020aedd83ae48737cadff4740beb5430abb
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-26 09:04:57 +0000

    Refactor Todo class
    Added validations to the properties to handle edge cases

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,13 +1,38 @@
 class Todo {
-  constructor(id, title, description, dueDate, priority, notes, checklist, completed, createdAt) {
-    this.id = id;
-    this.title = title;
+  constructor(id, title, description = "", dueDate, priority = "low", notes = "", checklist, completed, createdAt) {
+    const allowed = ["low", "medium", "high"];
+
+    this.id = id || crypto.randomUUID();
+
+    if (title) {
+      let trimTitle = title.trim();
+      this.title = trimTitle.length === 0 ? "Untitled" : trimTitle;
+    } else {
+      this.title = "Untitled";
+    }
+
     this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
+
+   
+    if (typeof dueDate === "string") {
+      const trimDueDate = dueDate.trim();
+      if (trimDueDate.length === 0) {
+        this.dueDate = null;
+      } else {
+        const tempDate = new Date(trimDueDate);
+        this.dueDate = !isNaN(tempDate.getTime()) ? tempDate : null;
+      }
+    } else if (Object.prototype.toString.call(dueDate) === "[object Date]") {
+      this.dueDate = !isNaN(dueDate.getTime()) ? dueDate : null;
+    } else {
+      this.dueDate = null;
+    }
+
+    this.priority = allowed.includes(priority) ? priority : "low";
+
     this.notes = notes;
-    this.checklist = checklist;
-    this.completed = completed;
-    this.createdAt = createdAt;
+    this.checklist = Array.isArray(checklist) ? checklist : [];
+    this.completed = completed !== undefined ? completed : false;
+    this.createdAt = createdAt || new Date();
   }
 }
