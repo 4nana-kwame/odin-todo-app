5e9e1dba64921f025b42f4fc9fa1e20b55f8d5d7
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-26 06:43:54 +0000

    Add a Todo class constructor
    Todo class will be responsible for creating objects with properties
    such as id, title, description, dueDate, priority, notes, checklist,
    completed, createdAt

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+class Todo {
+  constructor(id, title, description, dueDate, priority, notes, checklist, completed, createdAt) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+    this.completed = completed;
+    this.createdAt = createdAt;
+  }
+}
