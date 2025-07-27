32d83227f8d7f2625b0d8aff635fb33c33115305
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-27 21:43:26 +0000

    Add removeProject() method
    Takes an id, checks if id is present in #projects array and splice it
    and return true. Else return false.

--- src/services/todoManager.js
+++ src/services/todoManager.js
@@ -57,4 +57,15 @@ class ProjectManager {
       this.#projects.push(projectInstance);
     }
   }
-}  
+
+  removeProject(id) {
+    const projectIndex = this.#projects.findIndex(item => item.id === id);
+
+    if (projectIndex !== -1) {
+      this.#projects.splice(projectIndex, 1);
+      return true;
+    }
+
+    return false;
+  }
+}
