73aa264f2e66ad127be5c92c7b9805a087b73a4c
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-27 19:31:18 +0000

    Add private field and constructor
    Added a private field #projects to store all Project instances.
    Initialzed #projects as an empty array so it can accept a list of
    projects and convert them into Project instances

--- src/services/todoManager.js
+++ src/services/todoManager.js
@@ -1,3 +1,19 @@
 import { Project } from "./models/project.js";
 
-class ProjectManager {}
+class ProjectManager {
+  #projects
+
+  constructor (projects) {
+    this.#projects = [];
+
+    if (Array.isArray(projects)) {
+      for (let project of projects) {
+        if (project instanceof Project) {
+          this.#projects.push(project);
+        } else if (typeof project === "object") {
+          this.#projects.push(new Project(project.id, project.name, project.todos, project.createdAt));
+        }
+      }
+    }
+  }
+}
