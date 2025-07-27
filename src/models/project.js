cadeb92e692d049e5eddc4709aea22b4f9945506
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-27 03:35:47 +0000

    Add Project class
    Created a Project class and added private properties with validation

---
+++ src/models/project.js
@@ -0,0 +1,41 @@
+class Project {
+  #id;
+  #name;
+  #todos;
+  #createdAt;
+
+  constructor (id, name, todos, createdAt) {
+    this.#id = id || crypto.randomUUID();
+    
+    if (typeof name === "string") {
+      const trimmedName = name.trim();
+
+      this.#name = trimmedName.length === 0 ? "" : trimmedName;
+    }
+
+    this.#todos = [];
+    if (Array.isArray(todos)) {
+      todos.forEach(value => {
+        if (typeof value === "object" && value !== null) {
+          if (
+            typeof value.id === "string" &&
+            typeof value.text === "string" &&
+            typeof value.done === "boolean"
+          ) {
+            this.#todos.push(value);
+          }
+        } else if (typeof value === "string") {
+          const trimmedValue = value.trim();
+
+          this.#todos.push({
+            id: crypto.randomUUID(),
+            text: trimmedValue.length === 0 ? "" : trimmedValue,
+            done: false
+          });
+        }
+      });
+    }
+
+    this.#createdAt = createdAt || new Date();
+  }
+}
