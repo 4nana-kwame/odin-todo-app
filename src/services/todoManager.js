9ec151877cb8af02a7417b1839692b54b4fc7316
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-29 14:40:49 +0000

    Add constructor to TodoManager
    The constructor takes projectManager and validates if projectManager is
    an instance of ProjectManager

--- src/services/todoManager.js
+++ src/services/todoManager.js
@@ -3,7 +3,11 @@ import { ProjectManager } from "./projectManager.js";
 class TodoManager {
   #projectManager;
 
-  constructor (ProjectManager) {
-    
+  constructor (projectManager) {
+    if (projectManager instanceof ProjectManager) {
+      this.#projectManager = projectManager;
+    } else {
+      throw new Error("Invalid projectManager instance");
+    }
   }
 }
