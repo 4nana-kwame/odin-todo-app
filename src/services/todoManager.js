ca60776e7085bc5d98aae076613d844764bc1dd1
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-29 15:06:21 +0000

    Add createTodo() method
    This method takes a projectId and todoData, finds the project, creates a
    todo and adds it to the project, update it's markAsComplete() method and
    save it.

--- src/services/todoManager.js
+++ src/services/todoManager.js
@@ -1,4 +1,5 @@
 import { ProjectManager } from "./projectManager.js";
+import { Todo } from "../models/todo.js";
 
 class TodoManager {
   #projectManager;
@@ -9,5 +10,27 @@ class TodoManager {
     } else {
       throw new Error("Invalid projectManager instance");
     }
+  }
+
+  createTodo(projectId, todoData) {
+    const project = this.#projectManager.findProjectById(projectId);
+
+    if (!project) throw new Error("Project not found");
+
+    const todo = new Todo(
+      todoData.id,
+      todoData.title,
+      todoData.description,
+      todoData.dueDate,
+      todoData.priority,
+      todoData.notes,
+      todoData.checklist,
+      todoData.completed,
+      todoData.createdAt
+    );
+
+    project.addTodo(todo);
+    project.markAsComplete();
+    this.#projectManager.save();
   }
 }
