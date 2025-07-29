120fbb30e7e30c9550382cb5f412cdd244851876
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-29 23:54:36 +0000

    Add TodoManager class with constructor
    TodoManager constructor takes an object or array of objects and creates
    Todo instances

--- src/services/todoManager.js
+++ src/services/todoManager.js
@@ -1,3 +1,20 @@
 import { Todo } from "../models/todo.js";
 
+class TodoManager {
+  #todos;
 
+  constructor(initialTodos = []) {
+    this.#todos = [];
+
+    if (Array.isArray(initialTodos)) {
+      initialTodos.forEach(todoData => {
+        if (todoData instanceof Todo) {
+          this.#todos.push(todoData);
+        } else if (typeof initialTodos === "object" && initialTodos !== null) {
+          const restored = Todo.fromJSON(initialTodos);
+          this.#todos.push(restored);
+        }
+      });
+    }
+  }
+}
