3c2d49cdbed4248801dad25d987bb8ab135726f9
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-31 14:26:17 +0000

    Add contructor to set projects

--- src/services/projectManager.js
+++ src/services/projectManager.js
@@ -2,6 +2,10 @@ import { Project } from "../models/project.js";
 
 class ProjectManager {
   #projects;
+
+  constructor (projects) {
+    this.projects = projects;
+  }
 }
 
 export { ProjectManager };
