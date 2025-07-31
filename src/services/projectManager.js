56af0feeae90d5b976719b09a5c9166a1fee64d5
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-31 15:09:01 +0000

    Add getter and setter methods to access projects

--- src/services/projectManager.js
+++ src/services/projectManager.js
@@ -6,6 +6,26 @@ class ProjectManager {
   constructor (projects) {
     this.projects = projects;
   }
+
+  get projects() { return this.#projects.slice(); }
+
+  set projects(newProjects) {
+    this.#projects = [];
+
+    if (Array.isArray(newProjects)) {
+      for (let newProject of newProjects) {
+        if (newProject instanceof Project) {
+          this.#projects.push(newProject);
+        } else if (typeof newProject === "object" && newProject !== null) {
+          this.#projects.push(Project.fromJSON(newProject));
+        }
+      }
+    }
+
+    if (this.#projects.length === 0) {
+      this.#projects.push(new Project("Inbox", []));
+    }
+  }
 }
 
 export { ProjectManager };
