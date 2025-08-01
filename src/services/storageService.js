745e72e4e446c5ae4062289c03137b6145043cd6
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-01 14:24:34 +0000

    Add saveProjects() method
    Takes projectManager, validates it and saves to localstorage if no
    errors are found

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -1,5 +1,18 @@
-import { ProjectManager } from "./projectManager";
+import { ProjectManager } from "./projectManager.js";
 
 class StorageService {
   
+  saveProjects(projectManager) {
+    if (!projectManager || typeof projectManager.toJSON !== "function") {
+      throw new Error("saveProjects() expects a ProjectManager instance");
+    }
+
+    try {
+      const data = projectManager.toJSON();
+      const jsonString = JSON.stringify(data);
+      localStorage.setItem("projects", jsonString);
+    } catch (error) {
+      console.error("Failed to save projects", error);
+    }
+  }
 }
