4de4c458792356d8ca092be6c7f7b2452b941342
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-06 15:32:33 +0000

    Add class constructor with paramenters

--- src/models/todo.js
+++ src/models/todo.js
@@ -8,4 +8,24 @@ class Todo {
   #checklist;
   #completed;
   #createdAt;
+
+  constructor (
+    title = "New todo",
+    description = "",
+    dueDate,
+    priority = "low",
+    notes = "",
+    checklist = [],
+    completed = false,
+  ) {
+    this.#id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+    this.completed = completed;
+    this.#createdAt = new Date();
+  }
 }
