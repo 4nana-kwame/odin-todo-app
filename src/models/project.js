9adf14f64aec7eb4c5c80e896488f0f260821c12
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-07 00:48:14 +0000

    Add description and completed to constructor

--- src/models/project.js
+++ src/models/project.js
@@ -3,13 +3,17 @@ import { Todo } from "./todo.js";
 class Project {
   #id;
   #name;
+  #description;
   #todos;
   #createdAt;
+  #completed;
 
-  constructor (name = "New Project", todos = []) {
+  constructor (name = "New Project", description = "", todos = [], completed = false) {
     this.#id = crypto.randomUUID();
     this.#name = name;
+    this.#description = description;
     this.#todos = todos;
     this.#createdAt = new Date();
+    this.completed = completed;
   }
 }
