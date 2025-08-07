e7aadf4903e107b2d0c2884da681f107c75c728f
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-07 00:33:39 +0000

    Add constructor with name and todos as parameters

--- src/models/project.js
+++ src/models/project.js
@@ -5,4 +5,11 @@ class Project {
   #name;
   #todos;
   #createdAt;
+
+  constructor (name = "New Project", todos = []) {
+    this.#id = crypto.randomUUID();
+    this.#name = name;
+    this.#todos = todos;
+    this.#createdAt = new Date();
+  }
 }
