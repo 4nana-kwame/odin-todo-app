0387136446a914b62f2dfa51d03df9750c661c12
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-08 15:08:13 +0000

    Add getTodoById() method
    Takes an ID and returns a todo if a match is found
    Initialiazed private todos with new Map() instance to push created Todo
    instances in it.

--- src/services/todomanager.js
+++ src/services/todomanager.js
@@ -1,6 +1,7 @@
 import { Todo } from "../models/todo.js";
 
 class TodoManager {
+  #todos = new Map();
 
   createTodo(title, description, dueDate, priority, completed) {
     const todo = new Todo(
@@ -10,7 +11,14 @@ class TodoManager {
       priority,
       completed
     );
-    
+
+    this.#todos.set(todo.id, todo);
     return todo;
+  }
+
+  getTodoById(todoId) {
+    if (typeof todoId !== "string") throw new Error("ID must be a string");
+
+    return this.#todos.get(todoId) || null;
   }
 }
