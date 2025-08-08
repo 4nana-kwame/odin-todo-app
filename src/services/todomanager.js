786c7297e9a41271efd2bbc1bf24016b0d9f7cbd
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-08 16:31:54 +0000

    Add updateTodo() method
    Takes todo ID and update data and updates the todo if a match is found.
    The update data must be an object with the right keys

--- src/services/todomanager.js
+++ src/services/todomanager.js
@@ -25,4 +25,33 @@ class TodoManager {
   getAllTodos() {
     return Array.from(this.#todos.values());
   }
+
+  updateTodo(todoId, updateData) {
+    if (!this.#todos.has(todoId)) return null;
+    if (typeof updateData !== "object" || updateData === null) return null;
+
+    const todo = this.#todos.get(todoId);
+
+    if (updateData.title !== undefined) {
+      todo.title = updateData.title;
+    }
+
+    if (updateData.description !== undefined) {
+      todo.description = updateData.description;
+    }
+
+    if (updateData.dueDate !== undefined) {
+      todo.dueDate = updateData.dueDate;
+    }
+
+    if (updateData.priority !== undefined) {
+      todo.priority = updateData.priority;
+    }
+
+    if (updateData.completed !== undefined) {
+      todo.completed = updateData.completed;
+    }
+
+    return todo;
+  }
 }
