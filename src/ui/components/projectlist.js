9639875ca6fb8afaa8f748aa07fe808d6995b2b2
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-12 17:56:29 +0000

    Add private fields and constructor

--- src/ui/components/projectlist.js
+++ src/ui/components/projectlist.js
@@ -1,1 +1,10 @@
 import { ProjectManager } from "../src/services/projectmanager.js";
+
+class ProjectList {
+  #projectManager;
+  #activateProjectId = null;
+
+  constructor (projectManager) {
+    this.#projectManager = projectManager;
+  }
+}
