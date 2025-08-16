1b21aa820f77eaded425085a79cc7c08e7089425
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-08-16 18:08:49 +0000

    Refactor constructor and add methods

--- src/js/models/todo.js
+++ src/js/models/todo.js
@@ -3,13 +3,23 @@ class Todo {
   constructor (title, description, dueDate, priority) {
     this.id = crypto.randomUUID();
     this.completed = false;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
+    this.title = title || "Untitled";
+    this.description = description || "";
+    this.dueDate = dueDate || new Date().toISOString().split("T")[0];
+    this.priority = priority || "low";
+  }
+
+  updateDueDate(newDueDate) {
+    this.dueDate = newDueDate || this.dueDate;
   }
 
   toggleCompleted() {
-    return this.completed = !this.completed;
+    this.completed = !this.completed;
+    return this;
+  }
+
+  updatePriority(newPriority) {
+    const allowed = ["low", "medium", "high"];
+    this.priority = allowed.includes(newPriority) ? newPriority : this.priority;
   }
 }
